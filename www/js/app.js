dcf0563fa33fdcad6167e0d21bb3969fc61b6308
jessicarbcs <jessicareboucascarvalho@gmail.com>
2018-05-17 22:50:02 -0300

    configurações do firebase.

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,26 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+// Firebase auth snippet
+// var provider = new firebase.auth.GoogleAuthProvider();
+// firebase.auth().signInWithRedirect(provider).then(function () {
+//     firebase.auth().getRedirectResult().then(function (result) {
+//     console.log("sucesso",result);
+//     // This gives you a Google Access Token.
+//         // You can use it to access the Google API.
+//         var token = result.credential.accessToken;
+//         // The signed-in user info.
+//         var user = result.user;
+//         // ...
+//     }).catch(function (error) {
+//         console.log("erro",error);
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//     });
+// });
+
+
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ion-floating-menu', 'firebase'])
 
     .run(function ($ionicPlatform) {
